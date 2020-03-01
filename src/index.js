8f7bb3b55bd016a80730e1a0739aac6b334fa926
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-01 17:31:44 +1100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/styles.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
