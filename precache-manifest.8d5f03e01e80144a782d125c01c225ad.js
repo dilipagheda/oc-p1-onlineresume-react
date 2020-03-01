e5505b3652ad4655dca5bb5dceb5a8f2b536d7d0
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-01 17:34:17 +1100

    Updates

---
+++ precache-manifest.8d5f03e01e80144a782d125c01c225ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c58f98094425a89bff8a10dc6fdeff",
+    "url": "/oc-p1-onlineresume-react/index/index.html"
+  },
+  {
+    "revision": "54b0740f393bf23597c6",
+    "url": "/oc-p1-onlineresume-react/index/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b0a200fb3c98ded09f8a",
+    "url": "/oc-p1-onlineresume-react/index/static/css/main.ccd1d570.chunk.css"
+  },
+  {
+    "revision": "54b0740f393bf23597c6",
+    "url": "/oc-p1-onlineresume-react/index/static/js/2.b2561d26.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/oc-p1-onlineresume-react/index/static/js/2.b2561d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a200fb3c98ded09f8a",
+    "url": "/oc-p1-onlineresume-react/index/static/js/main.75384036.chunk.js"
+  },
+  {
+    "revision": "80fd5c53a8be1866a03d",
+    "url": "/oc-p1-onlineresume-react/index/static/js/runtime-main.74230c0c.js"
+  },
+  {
+    "revision": "461ce4e5bbc63173af3eb297283c6e70",
+    "url": "/oc-p1-onlineresume-react/index/static/media/me.461ce4e5.png"
+  }
+]);
