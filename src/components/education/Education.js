ca5f685395bbf3590def80d6d43f21197ed0cbe7
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-01 16:33:18 +1100

    completed components for Education section

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -3,12 +3,16 @@ import EducationItem from './EducationItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faGraduationCap} from '@fortawesome/free-solid-svg-icons';
 import {EDUCATION} from '../../data/education/education';
+import usePagination from '../../hooks/PaginationHooks';
 
 const Education = () => {
+    const [getCurrentPageData, showButtons] = usePagination(EDUCATION.items);
+
     return ( 
         <section class="resume-education-section">
             <h2 class="resume-education-section-title"><FontAwesomeIcon icon={faGraduationCap} className="education-icon" /> Education</h2>
-            {EDUCATION.items.map(item => <EducationItem {...item}/>)}
+            {getCurrentPageData().map(item => <EducationItem {...item}/>)}
+            {showButtons()}
         </section>
      );
 }
